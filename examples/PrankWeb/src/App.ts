d5e9c9f6836b94b07d8e683a621b4640ad54e2e5
Lukas Jendele <lukas.jendele@gmail.com>
2017-04-07 14:49:54 +0200

    Tutorial for David.

--- examples/PrankWeb/src/App.ts
+++ examples/PrankWeb/src/App.ts
@@ -22,8 +22,12 @@ namespace LiteMol.PrankWeb {
         return plugin;
     }
 
+    // Div that LiteMol mounts into.
     let appNode = document.getElementById('app') !
+    // Div that control panel mounts into.
     let pocketNode = document.getElementById('pocket-list') !
+
+    // Specify what data to display.
     let inputType: string = appNode.getAttribute("data-input-type") !
     let inputId: string = appNode.getAttribute("data-input-id") !
     App.render(create(appNode), inputType, inputId, pocketNode);
