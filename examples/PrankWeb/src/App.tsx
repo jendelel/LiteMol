9fcfafea78e692aa1b07ac09ee483189ea328c75
Lukas Jendele <lukas.jendele@gmail.com>
2017-05-05 16:35:33 +0200

    Control buttons

--- examples/PrankWeb/src/App.tsx
+++ examples/PrankWeb/src/App.tsx
@@ -6,42 +6,45 @@ namespace LiteMol.PrankWeb.App {
             <App plugin={plugin} inputType={inputType} inputId={inputId} />, target)
     }
 
-    export class App extends React.Component<{plugin:Plugin.Controller, inputId:string, inputType:string},
-        {isLoading?:boolean, error?:string, data?: DataLoader.PrankData }> {
+    export class App extends React.Component<{ plugin: Plugin.Controller, inputId: string, inputType: string },
+        { isLoading?: boolean, error?: string, data?: DataLoader.PrankData }> {
 
-        state = {isLoading:false, data: void 0, error: void 0};
+        state = { isLoading: false, data: void 0, error: void 0 };
 
         componentDidMount() {
             this.load();
         }
 
         load() {
-            this.setState({isLoading: true, error: void 0});
+            this.setState({ isLoading: true, error: void 0 });
             // Load data.
-            DataLoader.loadData(this.props.plugin, this.props.inputType,this.props.inputId)
+            DataLoader.loadData(this.props.plugin, this.props.inputType, this.props.inputId)
                 // Visualize the data.
-                .then((data)=>DataLoader.visualizeData(this.props.plugin, data))
+                .then((data) => DataLoader.visualizeData(this.props.plugin, data))
                 // Color the protein surface and cartoon.
-                .then((data)=>DataLoader.colorProteinFuture(this.props.plugin, data))
+                .then((data) => DataLoader.colorProteinFuture(this.props.plugin, data))
                 // Everything went well, change the loading state.
-                .then((data)=> this.setState({isLoading:false, data}))
+                .then((data) => this.setState({ isLoading: false, data }))
                 // Something went wrong, change the loading state and set the error msg.
-                .catch((e)=> this.setState({isLoading:false, error:'' + e}));                
+                .catch((e) => this.setState({ isLoading: false, error: '' + e }));
         }
-        
+
         render() {
-            if(this.state.data) {
-                // Data available, display pocket list.
-                return <PocketList data={this.state.data!} plugin={this.props.plugin}/>
+            if (this.state.data) {
+                // Data available, display controls and pocket list.
+                let controls: any[] = [];
+                controls.push(<ControlButtons inputId={this.props.inputId} inputType={this.props.inputType}/>)
+                controls.push(<PocketList data={this.state.data!} plugin={this.props.plugin} />)
+                return <div>{controls}</div>
             } else {
-                let controls : any[] = [];
+                let controls: any[] = [];
                 if (this.state.isLoading) {
                     controls.push(<h1>Loading...</h1>)
                 } else {
                     // Offer a button to load data.
-                    controls.push(<button onClick={()=>this.load()}>Load data</button>)
+                    controls.push(<button onClick={() => this.load()}>Load data</button>)
                     if (this.state.error) {
-                        controls.push(<div style={{color: 'red', fontSize:'18px'}} >Error: {this.state.error}</div>)
+                        controls.push(<div style={{ color: 'red', fontSize: '18px' }} >Error: {this.state.error}</div>)
                     }
                 }
                 return <div>{controls}</div>
